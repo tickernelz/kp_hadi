9e2ced1c56bb29ebdb2f85aca577041ffab366c0
tickernelz <zhafronadani@gmail.com>
2021-10-31 18:13:26 +0700

    tambah aturan roles pada side_menu
    Signed-off-by: tickernelz <zhafronadani@gmail.com>

--- app/Main/SideMenu.php
+++ app/Main/SideMenu.php
@@ -2,6 +2,7 @@
 
 namespace App\Main;
 
+use Auth;
 use Illuminate\Http\Request;
 
 class SideMenu
@@ -14,6 +15,195 @@ class SideMenu
      */
     public static function menu()
     {
+        if (Auth::check())
+        {
+            if (Auth::user()->hasRole('admin')) {
+                return [
+                    'dashboard' => [
+                        'icon' => 'home',
+                        'route_name' => 'beranda',
+                        'params' => [
+                        ],
+                        'title' => 'Dashboard',
+                    ],
+                    'devider',
+                    'kelola-kelas' => [
+                        'icon' => 'bell',
+                        'title' => 'Kelola Kelas',
+                        'params' => [
+                        ],
+                        'sub_menu' => [
+                            'kelas-list' => [
+                                'icon' => '',
+                                'route_name' => 'kelola.kelas',
+                                'params' => [
+                                ],
+                                'title' => 'Daftar Kelas',
+                            ],
+                            'kelas-tambah' => [
+                                'icon' => '',
+                                'route_name' => 'kelola.kelas.tambah',
+                                'params' => [
+                                ],
+                                'title' => 'Tambah Kelas',
+                            ],
+                        ],
+                    ],
+                    'kelola-tahun_ajaran' => [
+                        'icon' => 'calendar',
+                        'title' => 'Kelola Tahun Ajaran',
+                        'params' => [
+                        ],
+                        'sub_menu' => [
+                            'tahun_ajaran-list' => [
+                                'icon' => '',
+                                'route_name' => 'kelola.tahun_ajaran',
+                                'params' => [
+                                ],
+                                'title' => 'Daftar Tahun Ajaran',
+                            ],
+                            'tahun_ajaran-tambah' => [
+                                'icon' => '',
+                                'route_name' => 'kelola.tahun_ajaran.tambah',
+                                'params' => [
+                                ],
+                                'title' => 'Tambah Tahun Ajaran',
+                            ],
+                        ],
+                    ],
+                    'kelola-siswa' => [
+                        'icon' => 'smile',
+                        'title' => 'Kelola Siswa',
+                        'params' => [
+                        ],
+                        'sub_menu' => [
+                            'siswa-list' => [
+                                'icon' => '',
+                                'route_name' => 'kelola.siswa',
+                                'params' => [
+                                ],
+                                'title' => 'Daftar Siswa',
+                            ],
+                            'siswa-tambah' => [
+                                'icon' => '',
+                                'route_name' => 'kelola.siswa.tambah',
+                                'params' => [
+                                ],
+                                'title' => 'Tambah Siswa',
+                            ],
+                        ],
+                    ],
+                    'kelola-mata_pelajaran' => [
+                        'icon' => 'book-open',
+                        'title' => 'Kelola Mata Pelajaran',
+                        'params' => [
+                        ],
+                        'sub_menu' => [
+                            'mata_pelajaran-list' => [
+                                'icon' => '',
+                                'route_name' => 'kelola.mata_pelajaran',
+                                'params' => [
+                                ],
+                                'title' => 'Daftar Mata Pelajaran',
+                            ],
+                            'mata_pelajaran-tambah' => [
+                                'icon' => '',
+                                'route_name' => 'kelola.mata_pelajaran.tambah',
+                                'params' => [
+                                ],
+                                'title' => 'Tambah Mata Pelajaran',
+                            ],
+                        ],
+                    ],
+                    'kelola-wali_kelas' => [
+                        'icon' => 'briefcase',
+                        'title' => 'Kelola Wali Kelas',
+                        'params' => [
+                        ],
+                        'sub_menu' => [
+                            'wali_kelas-list' => [
+                                'icon' => '',
+                                'route_name' => 'kelola.wali_kelas',
+                                'params' => [
+                                ],
+                                'title' => 'Daftar Wali Kelas',
+                            ],
+                            'wali_kelas-tambah' => [
+                                'icon' => '',
+                                'route_name' => 'kelola.wali_kelas.tambah',
+                                'params' => [
+                                ],
+                                'title' => 'Tambah Wali Kelas',
+                            ],
+                        ],
+                    ],
+                    'kelola-kelompok_nilai' => [
+                        'icon' => 'archive',
+                        'title' => 'Kelola Kelompok Nilai',
+                        'params' => [
+                        ],
+                        'sub_menu' => [
+                            'kelompok_nilai-list' => [
+                                'icon' => '',
+                                'route_name' => 'kelola.kelompok_nilai',
+                                'params' => [
+                                ],
+                                'title' => 'Daftar Kelompok Nilai',
+                            ],
+                            'kelompok_nilai-tambah' => [
+                                'icon' => '',
+                                'route_name' => 'kelola.kelompok_nilai.tambah',
+                                'params' => [
+                                ],
+                                'title' => 'Tambah Kelompok Nilai',
+                            ],
+                        ],
+                    ],
+                    'kelola-nilai' => [
+                        'icon' => 'edit-3',
+                        'title' => 'Kelola Nilai',
+                        'params' => [
+                        ],
+                        'sub_menu' => [
+                            'nilai-list' => [
+                                'icon' => '',
+                                'route_name' => 'kelola.nilai',
+                                'params' => [
+                                ],
+                                'title' => 'Daftar Nilai',
+                            ],
+                        ],
+                    ],
+                ];
+            }
+            if (Auth::user()->hasRole('wali_kelas')) {
+                return [
+                    'dashboard' => [
+                        'icon' => 'home',
+                        'route_name' => 'beranda',
+                        'params' => [
+                        ],
+                        'title' => 'Dashboard',
+                    ],
+                    'devider',
+                    'kelola-nilai' => [
+                        'icon' => 'edit-3',
+                        'title' => 'Kelola Nilai',
+                        'params' => [
+                        ],
+                        'sub_menu' => [
+                            'nilai-list' => [
+                                'icon' => '',
+                                'route_name' => 'kelola.nilai',
+                                'params' => [
+                                ],
+                                'title' => 'Daftar Nilai',
+                            ],
+                        ],
+                    ],
+                ];
+            }
+        }
         return [
             'dashboard' => [
                 'icon' => 'home',
@@ -175,6 +365,28 @@ class SideMenu
                         ],
                         'title' => 'Tambah Kelompok Nilai',
                     ],
+                ],
+            ],
+            'kelola-nilai' => [
+                'icon' => 'edit-3',
+                'title' => 'Kelola Nilai',
+                'params' => [
+                ],
+                'sub_menu' => [
+                    'nilai-list' => [
+                        'icon' => '',
+                        'route_name' => 'kelola.nilai',
+                        'params' => [
+                        ],
+                        'title' => 'Daftar Nilai',
+                    ],
+//                    'nilai-tambah' => [
+//                        'icon' => '',
+//                        'route_name' => 'kelola.nilai.tambah',
+//                        'params' => [
+//                        ],
+//                        'title' => 'Tambah Nilai',
+//                    ],
                 ],
             ],
 //            'crud' => [
