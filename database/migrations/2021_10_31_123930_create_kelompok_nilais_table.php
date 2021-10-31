b8808349d92e072e1675648026f5e6abfab9f59c
tickernelz <zhafronadani@gmail.com>
2021-10-31 15:15:40 +0700

    tambah tabel 'kelompok nilai'
    Signed-off-by: tickernelz <zhafronadani@gmail.com>

---
+++ database/migrations/2021_10_31_123930_create_kelompok_nilais_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Kelas;
+use App\Models\MataPelajaran;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKelompokNilaisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kelompok_nilais', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(MataPelajaran::class);
+            $table->foreignIdFor(Kelas::class);
+            $table->string('nama_kelompok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kelompok_nilais');
+    }
+}
