7b02e18be02a144503d9b5acbac858b749dfbd84
tickernelz <zhafronadani@gmail.com>
2021-10-31 15:16:08 +0700

    tambah model 'kelompok nilai'
    Signed-off-by: tickernelz <zhafronadani@gmail.com>

---
+++ app/Models/KelompokNilai.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\KelompokNilai
+ *
+ * @property int $id
+ * @property int $mata_pelajaran_id
+ * @property int $kelas_id
+ * @property string $nama_kelompok
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Kelas $kelas
+ * @property-read \App\Models\MataPelajaran $mata_pelajaran
+ * @method static \Illuminate\Database\Eloquent\Builder|KelompokNilai newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|KelompokNilai newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|KelompokNilai query()
+ * @method static \Illuminate\Database\Eloquent\Builder|KelompokNilai whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|KelompokNilai whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|KelompokNilai whereKelasId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|KelompokNilai whereMataPelajaranId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|KelompokNilai whereNamaKelompok($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|KelompokNilai whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class KelompokNilai extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public function kelas()
+    {
+        return $this->belongsTo(Kelas::class);
+    }
+
+    public function mata_pelajaran()
+    {
+        return $this->belongsTo(MataPelajaran::class);
+    }
+}
